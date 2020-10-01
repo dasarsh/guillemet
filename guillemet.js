f40c5ceab7ea5553d2e0a5b5d2b14635a1bad27b
dasarsh <dasarsh@gmail.com>
2020-10-01 21:54:53 +1300

    Removed dotenv

--- guillemet.js
+++ guillemet.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4209;
@@ -11,6 +10,6 @@ app.get('/', function (req, res) {
     res.send('this is guillemet');
 })
 
-var server = app.listen(4209, function () {
+var server = app.listen(PORT, function () {
     console.log(`guillemet listening on ${ PORT }`)
 })
