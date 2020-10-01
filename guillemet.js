6fcec59f5d8c0579b0fcf827b6d62b9b6ad44ed7
dasarsh <dasarsh@gmail.com>
2020-10-01 21:11:07 +1300

    Initial commit

---
+++ guillemet.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('this is guillemet');
+})
+
+var server = app.listen(4209, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("guillemet listening at http://%s:%s", host, port)
+})
