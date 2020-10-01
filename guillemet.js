689e54656faa9d29deb39c0c37404ff6d2207797
dasarsh <dasarsh@gmail.com>
2020-10-01 21:16:33 +1300

    Added test endpoint

--- guillemet.js
+++ guillemet.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/roomy', function (req, res) {
+    res.send('this is roomy');
+})
+
 app.get('/', function (req, res) {
     res.send('this is guillemet');
 })
